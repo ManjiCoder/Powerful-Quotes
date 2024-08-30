18ce3cdcb75d9f24ae372e8dc4e9099ec146553b
Manji Coder <manjicoder@gmail.com>
2024-08-30 18:33:53 +0530

    Testing ✅

--- app/components/DaysInfo.tsx
+++ app/components/DaysInfo.tsx
@@ -7,7 +7,7 @@ import {
 } from 'date-fns';
 import { useLayoutEffect, useRef } from 'react';
 
-export default function DaysInfo() {
+const calculateTimeDiff = (targetElemt: HTMLDivElement | null) => {
   const startDate = new Date().setHours(0, 0, 0, 0);
   const days = differenceInDays(new Date(), startDate);
   const hrs = (differenceInHours(new Date(), startDate) % 24)
@@ -19,36 +19,53 @@ export default function DaysInfo() {
   const secs = (differenceInSeconds(new Date(), startDate) % 60)
     .toString()
     .padStart(2, '0');
+  // console.log({ days, hrs, mins, secs });
+  if (targetElemt) {
+    const [dd, hh, mm, ss] = Array.from(targetElemt.querySelectorAll('h2'));
+    // @ts-ignore
+    dd.querySelectorAll('span')[0].innerHTML = days;
+    hh.querySelectorAll('span')[0].innerHTML = hrs;
+    mm.querySelectorAll('span')[0].innerHTML = mins;
+    ss.querySelectorAll('span')[0].innerHTML = secs;
+  }
+};
+
+export default function DaysInfo() {
   const ref = useRef<HTMLDivElement>(null);
   useLayoutEffect(() => {
-    console.log(ref.current);
-    console.log({ days, hrs, mins, secs });
+    const targetElemt = ref.current;
+    const intervalId = setInterval(() => calculateTimeDiff(targetElemt), 1000);
+    return () => {
+      clearInterval(intervalId);
+    };
   }, []);
 
   return (
     <div ref={ref} className="flex flex-col place-items-center text-balance">
       <h1 className="mb-3 text-4xl md:text-6xl font-semibold">
-        <span className="text-mark">{days}</span> / 150{' '}
+        <span className="text-mark">{0}</span> / 150{' '}
         <span className="text-xl md:text-3xl">Days</span>{' '}
       </h1>
       <header className="flex place-items-center space-x-3 text-center">
         <div className="text-4xl">⌛</div>
 
         <h2 className="min-w-14 md:min-w-24 mb-3 mt-4 text-2xl font-semibold flex space-x-1 items-baseline">
-          <span className="text-mark">{days}</span>
-          <span className="text-base">{days > 2 ? 'Days' : 'Day'}</span>
+          <span className="text-mark">{0}</span>
+          <span className="text-base">{0 > 2 ? 'Days' : 'Day'}</span>
         </h2>
         <h2 className="min-w-14 md:min-w-24 mb-3 mt-4 text-2xl font-semibold flex space-x-1 items-baseline">
-          <span className="text-mark">{hrs}</span>
-          <span className="text-base">{parseInt(hrs) > 2 ? 'Hrs' : 'Hr'}</span>
+          <span className="text-mark">{0}</span>
+          <span className="text-base">{0 > 2 ? 'Hrs' : 'Hr'}</span>
         </h2>
         <h2 className="min-w-14 md:min-w-24 mb-3 mt-4 text-2xl font-semibold flex space-x-1 items-baseline">
-          <span className="text-mark">{mins}</span>
+          <span className="text-mark">{0}</span>
           {/* <span className="text-base">{mins > 2 ? 'Mins' : 'Min'}</span> */}
-          <span className="text-base">M</span>
+          <span className="text-base mins">M</span>
         </h2>
         <h2 className="min-w-14 md:min-w-24 mb-3 mt-4 text-2xl font-semibold flex space-x-1 items-baseline">
-          <span className="text-mark">{secs}</span>
+          <span suppressHydrationWarning className="text-mark">
+            {0}
+          </span>
           <span className="text-base">S</span>
         </h2>
       </header>
--- app/npc/page.tsx
+++ app/npc/page.tsx
@@ -10,7 +10,7 @@ export default async function NPC() {
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           {/* Get started by editing&nbsp; */}
 
-          <code className="font-mono font-bold">
+          <code className="max-sm:px-10 max-sm:text-balance max-sm:text-center leading-loose font-mono font-bold">
             If you want to shine like the ☀️ first 🔥burn like the ☀️
           </code>
         </p>
