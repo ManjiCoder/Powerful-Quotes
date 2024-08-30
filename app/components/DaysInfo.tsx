68471aac31910c46c241eccd37582900cb361f4f
Manji Coder <manjicoder@gmail.com>
2024-08-30 17:57:04 +0530

    ui ✅

--- app/components/DaysInfo.tsx
+++ app/components/DaysInfo.tsx
@@ -5,23 +5,47 @@ import {
   differenceInMinutes,
   differenceInSeconds,
 } from 'date-fns';
+import { useLayoutEffect, useRef } from 'react';
+
 export default function DaysInfo() {
   const startDate = new Date().setHours(0, 0, 0, 0);
   const days = differenceInDays(new Date(), startDate);
   const hrs = differenceInHours(new Date(), startDate) % 24;
   const mins = differenceInMinutes(new Date(), startDate) % 60;
   const secs = differenceInSeconds(new Date(), startDate) % 60;
-  console.log({ days, hrs, mins, secs });
+  const ref = useRef<HTMLDivElement>(null);
+  useLayoutEffect(() => {
+    console.log(ref.current);
+    console.log({ days, hrs, mins, secs });
+  }, []);
+
   return (
-    <div className="flex flex-col place-items-center text-balance">
-      <h1 className="mb-3 text-6xl font-semibold">
+    <div ref={ref} className="flex flex-col place-items-center text-balance">
+      <h1 className="mb-3 text-4xl md:text-6xl font-semibold">
         <span className="text-mark">{days}</span> / 150{' '}
-        <span className="text-5xl">Days</span>{' '}
+        <span className="text-xl md:text-3xl">Days</span>{' '}
       </h1>
-      <h2 className="mb-3  mt-4 text-2xl font-semibold">
-        Time Complete <span className="text-mark">{days}</span>{' '}
-        {days > 2 ? 'Days' : 'Day'}
-      </h2>
+      <header className="flex place-items-center space-x-3 text-center">
+        <div className="text-4xl">⌛</div>
+
+        <h2 className="mb-3 mt-4 text-2xl font-semibold flex space-x-1 items-baseline">
+          <span className="text-mark">{days}</span>
+          <span className="text-base">{days > 2 ? 'Days' : 'Day'}</span>
+        </h2>
+        <h2 className="mb-3 mt-4 text-2xl font-semibold flex space-x-1 items-baseline">
+          <span className="text-mark">{hrs}</span>
+          <span className="text-base">{hrs > 2 ? 'Hrs' : 'Hr'}</span>
+        </h2>
+        <h2 className="mb-3 mt-4 text-2xl font-semibold flex space-x-1 items-baseline">
+          <span className="text-mark">{mins}</span>
+          {/* <span className="text-base">{mins > 2 ? 'Mins' : 'Min'}</span> */}
+          <span className="text-base">M</span>
+        </h2>
+        <h2 className="mb-3 mt-4 text-2xl font-semibold flex space-x-1 items-baseline">
+          <span className="text-mark">{secs}</span>
+          <span className="text-base">S</span>
+        </h2>
+      </header>
     </div>
   );
 }
